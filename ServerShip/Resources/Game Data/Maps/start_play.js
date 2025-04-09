0c1fa3365722903754a97348c0e67750d4779a09
AnDrNew80 <andrzej.drab@gmail.com>
2025-04-09 13:44:53 +0200

    Add files via upload

---
+++ ServerShip/Resources/Game Data/Maps/start_play.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Andrzej on 2016-05-01.
+ */
+
+exports.name = "Home Town";
+
+//game maker room
+exports.room = "ad_game";
+
+exports.start_x = 320;
+exports.start_y = 320;
+
+exports.clients = [];
